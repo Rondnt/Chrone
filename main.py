bc01b2d8863eea290ae0bd6ac222ae5908ebc454
José Rondón <107903082+Rondnt@users.noreply.github.com>
2023-11-17 02:24:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from lexico import main
+from lexico import main as lexico_main
+#from semantico import main as semantico_main
 
 if __name__ == '__main__':
-    main()
+    # Ejecutar el analizador léxico
+    lexico_main()
+
+    # Finalmente, ejecutar el analizador semántico
+    #semantico_main()
