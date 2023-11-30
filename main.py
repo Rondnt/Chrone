19badbaa6fac1c92625157a7c37ead5a5d100c76
José Rondón <107903082+Rondnt@users.noreply.github.com>
2023-11-30 00:12:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from lexico import main as lexico_main
-#from semantico import main as semantico_main
+from generacion import GeneradorCodigo
 
-if __name__ == '__main__':
+def main():
     # Ejecutar el analizador léxico
     lexico_main()
 
+    # Crear un generador de código
+    generador = GeneradorCodigo(None)
+
+    # Generar código y guardar en el archivo
+    generador.generar_codigo(output_file="codigo_generado.asm")
+
     # Finalmente, ejecutar el analizador semántico
     #semantico_main()
+
+if __name__ == '__main__':
+    # Ejecutar la función main al ejecutar main.py
+    main()
